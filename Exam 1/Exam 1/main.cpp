bbc0fc7fdc630fc799fe2050fcb38838d120805e
Steven Mumford <beaverstever@Stevens-MacBook-Pro.local>
2016-10-05 20:56:56 -0700

    Set up and input a project for tomorrows exam

---
+++ Exam 1/Exam 1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Exam 1
+//
+//  Created by Steven Mumford on 10/5/16.
+//  Copyright © 2016 Steven Mumford. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    
+    
+    return 0;
+}
